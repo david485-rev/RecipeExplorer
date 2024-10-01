c8bcef000b72f723a6dff01cd274ad75983753c8
Ian-Hoole <hoolei@sonoma.edu>
2024-10-01 11:51:26 -0700

    adjusted coding style to match conventions

--- src/controllers/user-routes.js
+++ src/controllers/user-routes.js
@@ -7,28 +7,25 @@ require('dotenv').config();
 const secretKey = process.env.JWT_SECRET;
 
 router.post("/login", async (req, res) => {
-    // logger.info(req.body.username);
-    // logger.info(req.body.password);
-    let token = null;
-    if(req.body){
-        if (req.body.username && req.body.password) {
-            const account = await getUserByUsernamePassword(req.body.username, req.body.password);
-            if(account){
-                token = jwt.sign({
-                    uuid:account.uuid,
-                    username: account.username
-                }, secretKey, {
-                    expiresIn: "7d"
-                });
-            } 
-        }
+    try{
+        let token = null;
+        const account = await getUserByUsernamePassword(req.body.username, req.body.password);
+        
+        token = jwt.sign({
+            uuid: account.uuid,
+            username: account.username
+        }, secretKey, {
+            expiresIn: "7d"
+        });
+        res.status(200).json({ token });
+        return;
     }
-    if(token){
-        return res.status(200).json({token});
+    catch (err) {
+        logger.error(err);
+        res.status(400).json({ message: "no account found" });
+        return;
     }
-    else{
-        res.status(400).json({message: "no account found"});
-    }
+    
 })
 router.post('/register', async function(req, res, next) {
         try {
