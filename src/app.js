c4ad0537069e944ce3f8624b5d9120c3e0481034
Burak <14030652+BurakAy@users.noreply.github.com>
2024-09-30 17:12:31 -0700

    Created request handler for /recipes endpoint and importing controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-
+const recipesController = require("./controllers/RecipesController");
+const logger = require("./util/logger");
 const PORT = 3000;
+const path = require("path");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Incoming ${req.method} : ${req.url}`);
+  next();
+});
+
+app.use("/recipes", recipesController);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
