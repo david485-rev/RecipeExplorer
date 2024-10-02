56031f46ae86f18ccecc39ab5d7facffacb497e1
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-02 15:10:41 -0700

    Created request handler for getting recipes

--- src/controllers/recipe-routes.js
+++ src/controllers/recipe-routes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createRecipe } = require("../service/recipe-service");
+const { getRecipes, createRecipe } = require("../service/recipe-service");
 
 router.post("/", async (req, res) => {
-  let response = await createRecipe(req.body);
+  const response = await createRecipe(req.body);
   res.status(response.$metadata.httpStatusCode);
   res.send(response);
+});
+
+router.get("/", async (req, res) => {
+  const response = await getRecipes();
+  res.status(response.$metadata.httpStatusCode);
+  res.send(response.Items);
 });
 
 module.exports = router;
