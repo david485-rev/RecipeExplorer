1ef44952cea509c401eff774adf990ea59b3d93a
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-02 17:46:36 -0700

    Implemented error handling

--- src/service/recipe-service.js
+++ src/service/recipe-service.js
@@ -1,15 +1,26 @@
+const { logger } = require("../util/logger");
 const { queryRecipes, insertRecipe } = require("../repository/recipe-dao");
 const Recipe = require("../model/recipe");
 
-async function createRecipe(recipeData) {
-  const newRecipe = new Recipe(recipeData);
-  const recipe = await insertRecipe(newRecipe);
-  return recipe;
+async function getRecipes() {
+  try {
+    const recipes = await queryRecipes();
+    return recipes;
+  } catch (err) {
+    logger.error(err);
+    throw new Error(err);
+  }
 }
 
-async function getRecipes() {
-  const recipes = await queryRecipes();
-  return recipes;
+async function createRecipe(recipeData) {
+  try {
+    const newRecipe = new Recipe(recipeData);
+    const recipe = await insertRecipe(newRecipe);
+    return recipe;
+  } catch (err) {
+    logger.error(err);
+    throw new Error(err);
+  }
 }
 
 module.exports = { getRecipes, createRecipe };
