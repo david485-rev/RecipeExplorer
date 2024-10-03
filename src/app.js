4c36dab5f2c87ca135608fffb94c8c6c1be6da0e
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-02 17:55:32 -0700

    Changed variable name from recipesController to recipeRouter

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
-const recipesController = require("./controllers/recipe-routes.js");
-const path = require('path');
+const recipeRouter = require("./controllers/recipe-routes.js");
+const path = require("path");
 const userRouter = require("./controllers/user-routes.js");
-const { logger } = require("./util/logger.js"); 
+const { logger } = require("./util/logger.js");
 
 const PORT = 3000;
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    logger.info(`Incoming ${req.method} : ${req.url}`);
-    next();
+  logger.info(`Incoming ${req.method} : ${req.url}`);
+  next();
 });
 
-app.use("/recipes", recipesController);
+app.use("/recipes", recipeRouter);
 app.use("/users", userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`);
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
