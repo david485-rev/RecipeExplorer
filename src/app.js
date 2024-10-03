ed3c298cd812b4f8d4937c4b023b7a4fed01b56c
Ian-Hoole <hoolei@sonoma.edu>
2024-10-03 12:06:52 -0700

    added a general query function

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const recipeRouter = require("./controllers/recipe-routes.js");
 const userRouter = require("./controllers/user-routes.js");
 const commentRouter = require("./controllers/comment-routes.js");
+const generalService = require("./service/general-service.js");
 const { logger } = require("./util/logger.js"); 
 
 
@@ -18,6 +19,15 @@ app.use((req, res, next) => {
 app.use("/recipes", recipeRouter);
 app.use("/users", userRouter);
 app.use("/comments", commentRouter)
+app.get("/:uuid", async (req, res) => {
+    try{
+        const uuid = req.params.uuid;
+        const item = await generalService.getItemByUuid(uuid);
+        res.status(200).json(item);
+    }catch(err){
+        res.status(400).json({message: "error finding resource"});
+    }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
---
+++ src/service/general-service.js
@@ -0,0 +1,32 @@
+const { logger } = require('../util/logger');
+
+const { queryByUuid } = require('../repository/general-dao.js');
+
+async function getItemByUuid(uuid){
+    if(!uuid){
+        throw new Error('missing Uuid');
+    }
+    try{
+        const newItem = {};
+        const item = await queryByUuid(uuid);
+        //logger.info("item is: " + JSON.stringify(item));
+        var keys = Object.keys(item);
+        for (const key of keys) {
+            if(key !== 'password'){
+                if(item[key]){
+                    newItem[key] = item[key];
+                }
+            }
+        }
+        //logger.info("newItem is: " + JSON.stringify(newItem));
+        return newItem;
+    }
+    catch(err){
+        logger.error(err);
+        throw new Error(err);
+    }
+}
+
+module.exports = {
+    getItemByUuid
+}
