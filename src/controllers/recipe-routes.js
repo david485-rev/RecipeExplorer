e4fcbced4cbc70b17b357eaee73ce233717499d8
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-02 17:46:17 -0700

    Reordered handlers

--- src/controllers/recipe-routes.js
+++ src/controllers/recipe-routes.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { getRecipes, createRecipe } = require("../service/recipe-service");
 
-router.post("/", async (req, res) => {
-  const response = await createRecipe(req.body);
-  res.status(response.$metadata.httpStatusCode);
-  res.send(response);
-});
-
 router.get("/", async (req, res) => {
   const response = await getRecipes();
   res.status(response.$metadata.httpStatusCode);
   res.send(response.Items);
+});
+
+router.post("/", async (req, res) => {
+  const response = await createRecipe(req.body);
+  res.status(response.$metadata.httpStatusCode);
+  res.send(response);
 });
 
 module.exports = router;
