454daa4cbc631a43346f42ec468935157e9ca710
David Ho <davidho6308493@gmail.com>
2024-10-03 06:53:21 -0700

    added type attribute to User class

--- src/model/user.js
+++ src/model/user.js
@@ -4,6 +4,7 @@ class User {
     constructor(username, password, description=null) {
         this.uuid = uuid.v4();
         this.creation_date = Math.floor(new Date().getTime() / 1000);
+        this.type = 'User';
         this.username = username;
         this.password = password;
         this.description = description;
