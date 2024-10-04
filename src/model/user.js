566cb4f6c94f9f9a7d0d92b4a407b6b21d951379
David Ho <davidho6308493@gmail.com>
2024-10-04 06:30:55 -0700

    changing User to user

--- src/model/user.js
+++ src/model/user.js
@@ -4,7 +4,7 @@ class User {
     constructor(username, password, description=null) {
         this.uuid = uuid.v4();
         this.creation_date = Math.floor(new Date().getTime() / 1000);
-        this.type = 'User';
+        this.type = 'user';
         this.username = username;
         this.password = password;
         this.description = description;
