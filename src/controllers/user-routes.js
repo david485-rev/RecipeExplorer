400fd495980efbcd5366d30c5a2eaedc0d28f89e
Eugene Na <eugene486@revature.net>
2024-10-04 11:46:43 -0500

    Revert "authentication to util and add password change"
    This reverts commit d87fa6114e79c5eeed7c8c69eb0a61a4e81590e4.
    
    	changing back to how main was

--- src/controllers/user-routes.js
+++ src/controllers/user-routes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const { logger } = require('../util/logger.js');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const { getUserByUsernamePassword, register, createProfile, passwordChange} = require('../service/user-service.js');
-const { getItemByUuid } = require('../service/general-service.js');
+const { getUserByUsernamePassword, register, createProfile, getInfoProfile} = require('../service/user-service.js');
 require('dotenv').config();
 const secretKey = process.env.JWT_SECRET;;
 
@@ -44,7 +43,7 @@ router.post('/register', async function(req, res, next) {
 router.get("/profile", authenticateToken, async(req, res)=> {
         const user = req.user;
         try{
-            const data = await getItemByUuid(user.uuid);
+            const data = await getInfoProfile(user.uuid);
             res.status(200).json(data);
         }catch(err){
             logger.error(err.message);
@@ -63,17 +62,6 @@ router.post("/profile", authenticateToken, async(req, res) => {
             
         }  
     }) 
-
-    router.patch("/password", authenticateToken, async(req, res) => {
-        const user = req.user;
-        try{
-            const data = await passwordChange(req.body, user.uuid, user.creation_date);
-            res.status(201).json({message: 'Sucessfully updated'});
-        } catch(err) {
-            logger.error(err.message);
-            res.status(400).json({message: err.message});
-        }
-    })    
 
 
 module.exports = router;
--- src/repository/user-dao.js
+++ src/repository/user-dao.js
@@ -77,30 +77,6 @@ async function queryByUuid(uuid) {
     }
 } 
 
-// update password
-async function patchPassword(item, uuid, creation_date) {
-    const command = new UpdateCommand({
-        TableName: "RecipeExplorer",
-        Key: { 
-            'uuid' :uuid,
-            'creation_date':creation_date
-        },
-          UpdateExpression:'Set #password = :password',
-          ExpressionAttributeNames: {
-            '#password': 'password'
-          },
-          ExpressionAttributeValues: {
-            ':password': item
-          },
-    });
-    try{
-        const data = await documentClient.send(command);
-        return data;
-    }catch(err) {
-        console.error(err);
-        throw new Error(err);
-    }
-} 
 
 // update profile
 async function postProfile(item, uuid, creation_date) {
@@ -139,6 +115,5 @@ module.exports = {
     createUser,
     queryUserByUsername,
     postProfile,
-    queryByUuid,
-    patchPassword
+    queryByUuid
 }
