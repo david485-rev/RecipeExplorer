1e821f5d04ffc2999fcb347c205bf1f67d997fa8
David Ho <davidho6308493@gmail.com>
2024-10-04 09:33:27 -0700

    added creating fake comments and modified the creation of fake recipes

---
+++ src/util/mock_data.js
@@ -0,0 +1,35 @@
+module.exports = {
+    ingredients: [
+        "chuck roast",
+        "potatoes",
+        "carrots",
+        "onions",
+        "tomato paste",
+        "salt",
+        "black pepper",
+        "red pepper flakes",
+        "olive oil",
+        "flour",
+        "water",
+        "vegetable oil",
+        "sugar",
+        "milk",
+        "vanilla extract",
+        "baking powder",
+        "sald"
+    ],
+    cuisine: [
+        "Italian",
+        "American",
+        "European",
+        "Japanese",
+        "Mexican",
+        "Asian"
+    ],
+    descriptions: [
+        "A flavorful, juicy, and fall apart tender meal for the family",
+        "Fluffy and buttery!",
+        "A heart warming soup to fill your belly!"
+    ],
+
+}
