d89e06ba57af456bcc6d3711843746da0a20781d
David Ho <davidho6308493@gmail.com>
2024-10-07 11:41:07 -0700

    formatting code

--- src/controllers/user-routes.js
+++ src/controllers/user-routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const { logger} = require('../util/logger.js');
-const { authenticateToken} = require('../util/authentication.js')
+const { logger } = require('../util/logger.js');
+const { authenticateToken } = require('../util/authentication.js')
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const { getUserByUsernamePassword, register, createProfile, passwordChange} = require('../service/user-service.js');
+const { getUserByUsernamePassword, register, createProfile, passwordChange } = require('../service/user-service.js');
 const { getItemByUuid } = require('../service/general-service.js');
 require('dotenv').config();
 const secretKey = process.env.JWT_SECRET;;
 
 router.post("/login", async (req, res) => {
-    try{
+    try {
         let token = null;
         const account = await getUserByUsernamePassword(req.body.username, req.body.password);
         token = jwt.sign({
@@ -27,54 +27,54 @@ router.post("/login", async (req, res) => {
         res.status(400).json({ message: "no account found" });
         return;
     }
-    
+
 })
-router.post('/register', async function(req, res, next) {
-        try {
-            await register(req.body);
+router.post('/register', async function (req, res, next) {
+    try {
+        await register(req.body);
 
-            res.status(201).json({ message: 'User successfully registered!' });
-        } catch(err) {
-            logger.error(err.message);
-            res.status(400).json({ message: err.message });
-            return;
-        }
-    })
+        res.status(201).json({ message: 'User successfully registered!' });
+    } catch (err) {
+        logger.error(err.message);
+        res.status(400).json({ message: err.message });
+        return;
+    }
+})
 
 
-router.get("/profile", authenticateToken, async(req, res)=> {
-        const user = req.user;
-        try{
-            const data = await getItemByUuid(user.uuid);
-            res.status(200).json(data);
-        }catch(err){
-            logger.error(err.message);
-            res.status(400).json({message: err.message});
-        }
-    });
+router.get("/profile", authenticateToken, async (req, res) => {
+    const user = req.user;
+    try {
+        const data = await getItemByUuid(user.uuid);
+        res.status(200).json(data);
+    } catch (err) {
+        logger.error(err.message);
+        res.status(400).json({ message: err.message });
+    }
+});
 
-router.post("/profile", authenticateToken, async(req, res) => {
-        const user = req.user;
-        try{
-            const data = await createProfile(req.body, user.uuid, user.creation_date);
-            res.status(201).json({message: 'Successfully updated'});
-        } catch(err) {
-            logger.error(err.message);
-            res.status(400).json({message: err.message});
-            
-        }  
-    }) 
+router.post("/profile", authenticateToken, async (req, res) => {
+    const user = req.user;
+    try {
+        const data = await createProfile(req.body, user.uuid, user.creation_date);
+        res.status(201).json({ message: 'Successfully updated' });
+    } catch (err) {
+        logger.error(err.message);
+        res.status(400).json({ message: err.message });
 
-    router.patch("/password", authenticateToken, async(req, res) => {
-        const user = req.user;
-        try{
-            const data = await passwordChange(req.body, user.uuid, user.creation_date);
-            res.status(201).json({message: 'Sucessfully updated'});
-        } catch(err) {
-            logger.error(err.message);
-            res.status(400).json({message: err.message});
-        }
-    })    
+    }
+})
+
+router.patch("/password", authenticateToken, async (req, res) => {
+    const user = req.user;
+    try {
+        const data = await passwordChange(req.body, user.uuid, user.creation_date);
+        res.status(201).json({ message: 'Sucessfully updated' });
+    } catch (err) {
+        logger.error(err.message);
+        res.status(400).json({ message: err.message });
+    }
+})
 
 
 module.exports = router;
