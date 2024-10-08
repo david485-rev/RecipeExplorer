4283564a0ae309056d123acfd63bb3f6a45592fa
Eugene Na <eugene486@revature.net>
2024-10-08 06:58:10 -0500

    password change test updated

--- __tests__/user-service.test.js
+++ __tests__/user-service.test.js
@@ -1,5 +1,7 @@
-const { createUser, queryUserByUsername} = require('../src/repository/user-dao');
-const { register } = require('../src/service/user-service');
+const { createUser, queryUserByUsername, patchPassword} = require('../src/repository/user-dao');
+const { register, passwordChange } = require('../src/service/user-service');
+const { getItemByUuid } = require('../src/repository/general-dao');
+const bcrypt = require("bcrypt");
 
 jest.mock('../src/repository/user-dao', () => {
     const originalModule = jest.requireActual('../src/repository/user-dao');
@@ -8,8 +10,18 @@ jest.mock('../src/repository/user-dao', () => {
         ...originalModule,
         queryUserByUsername: jest.fn(),
         createUser: jest.fn(),
+        patchPassword: jest.fn(),
     }
 });
+
+jest.mock('../src/repository/general-dao', () => {
+    const origin = jest.requireActual('../src/repository/general-dao');
+
+    return {
+        ...origin,
+        getItemByUuid: jest.fn()
+    }
+})
 
 describe('User Service Tests', () => {
     afterEach(() => {
@@ -54,23 +66,58 @@ describe('User Service Tests', () => {
         }).rejects.toThrow('missing password');
     });
 
-    test('register should throw an Error when trying to register an already existing username', async () => {
-        const reqBody = {
-            username: 'david123',
-            password: 'david'
-        };
 
-        queryUserByUsername.mockReturnValueOnce({
-            password: 'david',
-            username: 'david123',
-            join_date: 1726686455,
-            employee_id: '52caac7a-e48f-4587-9eac-c87422f4ba89'
-        });
+});
+
+describe('User Service, password test', () => {
+    afterEach(() => {
+        getItemByUuid.mockClear();
+    })
+
+    test('pasword should throw error if new password is empty', async() => {
+        const reqBody = {
+            password: 'rightPassword',
+            newPassword:""
+        }
 
         expect(async () => {
-            await register(reqBody);
-        }).rejects.toThrow('user with username already exists!');
-        expect(queryUserByUsername).toHaveBeenCalledTimes(1);
+            await passwordChange(reqBody); 
+        }).rejects.toThrow("New password can not be empty");
+        expect(getItemByUuid).toHaveBeenCalledTimes(1);
     });
+
+    test('password should throw error if typed password is not matched with a current password in database', async() => {
+        const reqBody ={
+            password: 'wrongPassword',
+            newPassword: 'newPassowrd'
+        };
+
+        getItemByUuid.mockReturnValueOnce({
+            password: 'rightpassword'
+        });
+
+        expect(async() => {
+            await passwordChange(reqBody);
+        }).rejects.toThrow("password is not correct");
+        expect(getItemByUuid).toHaveBeenCalledTimes(1);
+    })
+
+    test('metadata should be return upon successful change of password', async() => {
+        const encyptedPassword = await bcrypt.hash('rightPassword', 10);
+        const reqBody = {
+            password: 'rightPassword',
+            newPassword: 'newPassword'
+        }
+
+        getItemByUuid.mockReturnValueOnce({
+            password: encyptedPassword
+        });
+
+        const result = await passwordChange(reqBody);
+
+        expect(result).not.toBe(null);
+        expect(patchPassword).toHaveBeenCalledTimes(1);
+        expect(getItemByUuid).toHaveBeenCalledTimes(1);
+    })
     
 });
