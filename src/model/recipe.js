5e16967a29b9b9718ce954218aa54ef774f226ec
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-07 20:28:36 -0700

    Added author_id property to Recipe

--- src/model/recipe.js
+++ src/model/recipe.js
@@ -1,9 +1,10 @@
 const uuid = require("uuid");
 
 class Recipe {
-  constructor(recipeData) {
+  constructor(recipeData, authorId) {
     this.uuid = uuid.v4();
     this.creation_date = Math.floor(new Date().getTime() / 1000);
+    this.author_id = authorId;
     this.recipe_thumb = recipeData.recipe_thumb;
     this.recipe_name = recipeData.recipe_name;
     this.type = "recipe";
