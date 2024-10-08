9886bc51ec8b4a00f3de10e5f0ec36890bb56a19
David Ho <davidho6308493@gmail.com>
2024-10-08 08:40:54 -0700

    fixed register tests, added email check for /register

--- __tests__/user-service.test.js
+++ __tests__/user-service.test.js
@@ -1,4 +1,4 @@
-const { createUser, queryUserByUsername } = require('../src/repository/user-dao');
+const { createUser, queryUserByUsername, queryEmail } = require('../src/repository/user-dao');
 const { register } = require('../src/service/user-service');
 
 jest.mock('../src/repository/user-dao', () => {
@@ -7,6 +7,7 @@ jest.mock('../src/repository/user-dao', () => {
     return {
         ...originalModule,
         queryUserByUsername: jest.fn(),
+        queryEmail: jest.fn(),
         createUser: jest.fn(),
     }
 });
@@ -20,7 +21,10 @@ describe('User Service Tests', () => {
     test('register should return metadata on a successful register', async () => {
         const mockRequestBody = {
             username: 'david',
-            password: 'david'
+            password: 'david',
+            email: 'david@gmail.com',
+            description: 'this can be null',
+            picture: 'this is optional'
         };
 
         queryUserByUsername.mockReturnValueOnce(false);
@@ -35,7 +39,10 @@ describe('User Service Tests', () => {
     test('register should throw an Error when trying to register without a username', async () => {
         const reqBody = {
             username: null,
-            password: 'david123'
+            password: 'david123',
+            email: 'david@gmail.com',
+            description: 'this can be null',
+            picture: 'this is optional'
         };
 
         expect(async () => {
@@ -46,7 +53,10 @@ describe('User Service Tests', () => {
     test('register should throw an Error when trying to register without a password', async () => {
         const reqBody = {
             username: 'david123',
-            password: null
+            password: null,
+            email: 'david@gmail.com',
+            description: 'this can be null',
+            picture: 'this is optional'
         };
 
         expect(async () => {
@@ -54,16 +64,34 @@ describe('User Service Tests', () => {
         }).rejects.toThrow('missing password');
     });
 
+    test('register should throw an Error when trying to register without an email', async () => {
+        const reqBody = {
+            username: 'david123',
+            password: 'david123',
+            email: null,
+            description: 'this can be null',
+            picture: 'this is optional'
+        };
+
+        expect(async () => {
+            await register(reqBody);
+        }).rejects.toThrow('missing email');
+    });
+
     test('register should throw an Error when trying to register an already existing username', async () => {
         const reqBody = {
             username: 'david123',
-            password: 'david'
+            password: 'david',
+            email: 'david@gmail.com',
+            description: 'this can be null',
+            picture: 'this is optional'
         };
 
         queryUserByUsername.mockReturnValueOnce({
             password: 'david',
             username: 'david123',
-            join_date: 1726686455,
+            email: 'david@gmail.com',
+            creation_date: 1726686455,
             employee_id: '52caac7a-e48f-4587-9eac-c87422f4ba89'
         });
 
@@ -73,5 +101,23 @@ describe('User Service Tests', () => {
         expect(queryUserByUsername).toHaveBeenCalledTimes(1);
     });
 
+    test('register should throw an Error when trying to register with an email that is already used', async () => {
+        const reqBody = {
+            username: 'david123',
+            password: 'david',
+            email: 'david@gmail.com',
+            description: 'this can be null',
+            picture: 'this is optional'
+        };
 
+        queryEmail.mockReturnValueOnce({
+            email: 'david@gmail.com',
+            uuid: '04d196d1-7420-4da8-abd6-0e40aba3fd95'
+        });
+
+        expect(async () => {
+            await register(reqBody);
+        }).rejects.toThrow('email used already');
+        expect(queryUserByUsername).toHaveBeenCalledTimes(1);
+    });
 });
