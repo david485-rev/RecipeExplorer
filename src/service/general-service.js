7c38df8b7701ac5c92e6c9087980c887a6f30314
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-10 09:04:17 -0700

    Handling invalid uuid

--- src/service/general-service.js
+++ src/service/general-service.js
@@ -1,30 +1,34 @@
-const { logger } = require('../util/logger');
+const { logger } = require("../util/logger");
 
-const { getItemByUuid } = require('../repository/general-dao.js');
+const { getItemByUuid } = require("../repository/general-dao.js");
 
-async function getDatabaseItem(uuid){
-    if(!uuid){
-        throw new Error('missing Uuid');
+async function getDatabaseItem(uuid) {
+  if (!uuid) {
+    throw new Error("missing Uuid");
+  }
+  try {
+    const newItem = {};
+    const item = await getItemByUuid(uuid);
+
+    if (!item) {
+      throw new Error("invalid uuid");
     }
-    try{
-        const newItem = {};
-        const item = await getItemByUuid(uuid);
-        //logger.info("item is: " + JSON.stringify(item));
-        var keys = Object.keys(item);
-        for (const key of keys) {
-            if(key !== 'password'){
-                newItem[key] = item[key];
-            }
-        }
-        //logger.info("newItem is: " + JSON.stringify(newItem));
-        return newItem;
+
+    //logger.info("item is: " + JSON.stringify(item));
+    var keys = Object.keys(item);
+    for (const key of keys) {
+      if (key !== "password") {
+        newItem[key] = item[key];
+      }
     }
-    catch(err){
-        logger.error(err);
-        throw new Error(err);
-    }
+    //logger.info("newItem is: " + JSON.stringify(newItem));
+    return newItem;
+  } catch (err) {
+    logger.error(err);
+    throw new Error(err);
+  }
 }
 
 module.exports = {
-    getDatabaseItem
-}
+  getDatabaseItem
+};
