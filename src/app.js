52d38acbdcfb9e7da9d5517534f127626c50264c
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-10 12:54:05 -0700

    Setting up cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const recipeRouter = require("./controllers/recipe-routes.js");
 const userRouter = require("./controllers/user-routes.js");
 const commentRouter = require("./controllers/comment-routes.js");
 const { getDatabaseItem } = require("./service/general-service.js");
-const { logger } = require("./util/logger.js"); 
+const { logger } = require("./util/logger.js");
 
-
-const PORT = 3000;
+const PORT = 8888;
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   logger.info(`Incoming ${req.method} : ${req.url}`);
@@ -18,18 +19,17 @@ app.use((req, res, next) => {
 
 app.use("/recipes", recipeRouter);
 app.use("/users", userRouter);
-app.use("/comments", commentRouter)
+app.use("/comments", commentRouter);
 app.get("/:uuid", async (req, res) => {
-    try{
-        const uuid = req.params.uuid;
-      const item = await getDatabaseItem(uuid);
-        res.status(200).json(item);
-    }catch(err){
-        res.status(400).json({message: "error finding resource"});
-    }
-})
+  try {
+    const uuid = req.params.uuid;
+    const item = await getDatabaseItem(uuid);
+    res.status(200).json(item);
+  } catch (err) {
+    res.status(400).json({ message: "error finding resource" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
-
